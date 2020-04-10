206781feeb35088430b760b7470435d40ba44eee
Hendrik Mans <hendrik@mans.de>
2020-04-10 13:08:10 +0200

    Add actual test

--- packages/math/test/clamp.test.ts
+++ packages/math/test/clamp.test.ts
@@ -1,5 +1,15 @@
-const add = (a: number, b: number) => a + b;
+import { clamp, clamp01 } from "../src/clamp";
 
-test("adds numbers", () => {
-  expect(add(1, 2)).toBe(3);
+describe("clamp", () => {
+  test("respects minimum boundary", () => expect(clamp(3, 4, 6)).toBe(4));
+  test("respects maximum boundary", () => expect(clamp(7, 4, 6)).toBe(6));
+  test("returns the value when within boundaries", () =>
+    expect(clamp(5, 4, 6)).toBe(5));
+});
+
+describe("clamp01", () => {
+  test("respects minimum boundary", () => expect(clamp01(-1)).toBe(0));
+  test("respects maximum boundary", () => expect(clamp01(2)).toBe(1));
+  test("returns the value when within boundaries", () =>
+    expect(clamp01(0.5)).toBe(0.5));
 });
