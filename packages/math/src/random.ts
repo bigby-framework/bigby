08d3a17e79e8644df82be8d67d08c43e27b5532c
Hendrik Mans <hendrik@mans.de>
2020-04-10 14:31:08 +0200

    Add some codedocs

--- packages/math/src/random.ts
+++ packages/math/src/random.ts
@@ -1,11 +1,26 @@
+/**
+ * Returns a randomly generated number between 0 (inclusive) and the specified boundary (exclusive).
+ * @param max Upper boundary (exclusive) of random number to generate.
+ */
+export const number = (max = 1) => Math.random() * max;
+
+/**
+ * Returns a random number based on a specific value with a random offset applied.
+ *
+ * ```
+ * // returns a random number between 400 and 600
+ * variance(500, 100)
+ * ```
+ *
+ * @param base The base value.
+ * @param amplitude The amplitude of the random offset applied to the base value.
+ */
 export const variance = (base: number, amplitude: number) =>
   base + minusPlus(amplitude);
 
 export const minusPlus = (range = 1) => between(-range, range);
 
 export const minusPlusOne = () => minusPlus(1);
-
-export const number = (max = 1) => Math.random() * max;
 
 export const between = (min: number, max: number) => min + number(max - min);
 
