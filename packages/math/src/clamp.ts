195b0d3318aaa4cc1eb58abd46248ab33d1357d3
Hendrik Mans <hendrik@mans.de>
2020-04-10 12:38:03 +0200

    Add some codedocs

--- packages/math/src/clamp.ts
+++ packages/math/src/clamp.ts
@@ -1,4 +1,16 @@
+/**
+ * Clamps the given value between a minimum and maximum boundary.
+ *
+ * @param value The number to clamp between the given boundaries.
+ * @param min Minimum boundary.
+ * @param max Maximum boundary.
+ */
 export const clamp = (value: number, min: number, max: number) =>
   Math.min(Math.max(value, min), max);
 
+/**
+ * Clamps the given value between 0 and 1.
+ *
+ * @param value The number to clamp.
+ */
 export const clamp01 = (value: number) => clamp(value, 0, 1);
