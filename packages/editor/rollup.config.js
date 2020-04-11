758c4b69a110bef3a2654a73911743cce6a764be
Hendrik Mans <hendrik@mans.de>
2020-04-11 17:59:39 +0200

    Baby steps with rollup

---
+++ packages/editor/rollup.config.js
@@ -0,0 +1,10 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+  },
+  plugins: [typescript({ tsconfig: "tsconfig.build.json" })],
+};
