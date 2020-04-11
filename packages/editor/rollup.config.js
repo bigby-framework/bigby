eb08a9f2be244cad3d52bf9a7516835683998a51
Hendrik Mans <hendrik@mans.de>
2020-04-11 18:04:51 +0200

    baby steeeeps

--- packages/editor/rollup.config.js
+++ packages/editor/rollup.config.js
@@ -1,10 +1,17 @@
 import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
 export default {
   input: "src.test/index.ts",
-  output: {
-    file: "dist/index.js",
-    format: "cjs",
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es", // the preferred format
+    },
+  ],
   plugins: [typescript({ tsconfig: "tsconfig.build.json" })],
 };
