872494f062cb6a26bbb8abaac293160a884b539c
Hendrik Mans <hendrik@mans.de>
2020-04-12 19:55:30 +0200

    Add html rollup plugin

--- packages/sample-game/rollup.config.js
+++ packages/sample-game/rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from "rollup-plugin-typescript";
+import html from "@rollup/plugin-html";
 
 export default {
   input: "./src/index.tsx",
@@ -9,6 +10,7 @@ export default {
   },
   plugins: [
     typescript(),
+    html(),
     // resolve(), // tells Rollup how to find date-fns in node_modules
     // commonjs(), // converts date-fns to ES modules
     // production && terser() // minify, but only in production
