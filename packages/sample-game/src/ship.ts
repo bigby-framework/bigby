9eab9ce2c84d5f34df8e4b9a0423b40232675802
Hendrik Mans <hendrik@mans.de>
2020-04-12 21:24:58 +0200

    Okay, so this works now...

--- packages/sample-game/src/ship.ts
+++ packages/sample-game/src/ship.ts
@@ -2,8 +2,7 @@ import { Entity } from "@bigby/core";
 import { Renderable2D, AutoRotate2D, Sprite2D } from "@bigby/2d";
 
 export default () => {
-  const ship = new Entity();
-  ship.name = "Spaceship";
+  const ship = new Entity("Spaceship");
   ship.icon = "🕹";
   ship.addBehavior(Renderable2D);
   ship.addBehavior(Sprite2D, { uri: "/assets/lemming.png" });
