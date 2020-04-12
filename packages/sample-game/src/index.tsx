aef5fdd1b6496996918919cde7c653166df06131
Hendrik Mans <hendrik@mans.de>
2020-04-12 20:10:49 +0200

    Remove le dist

--- packages/sample-game/src/index.tsx
+++ packages/sample-game/src/index.tsx
@@ -1,1 +1,7 @@
-console.log("I LIVE");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const el = document.createElement("div");
+document.body.appendChild(el);
+
+ReactDOM.render(<p>hi</p>, el);
