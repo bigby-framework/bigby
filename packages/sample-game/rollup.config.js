2b23c4cbbf0b982bee9125341b02805171b1694b
Hendrik Mans <hendrik@mans.de>
2020-04-12 20:07:32 +0200

    Get a rollup thing going, it's nice

--- packages/sample-game/rollup.config.js
+++ packages/sample-game/rollup.config.js
@@ -1,5 +1,11 @@
 import typescript from "rollup-plugin-typescript";
 import html from "@rollup/plugin-html";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
+const development = !production;
 
 export default {
   input: "./src/index.tsx",
@@ -11,6 +17,9 @@ export default {
   plugins: [
     typescript(),
     html(),
+    development && serve("dist"),
+    development && livereload(),
+    production && terser(),
     // resolve(), // tells Rollup how to find date-fns in node_modules
     // commonjs(), // converts date-fns to ES modules
     // production && terser() // minify, but only in production
