2ae2a4704887319eea9f2604d561c95ffa31bebd
Hendrik Mans <hendrik@mans.de>
2020-04-12 20:34:40 +0200

    It works!

--- packages/sample-game/src/index.tsx
+++ packages/sample-game/src/index.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const el = document.createElement("div");
-document.body.appendChild(el);
+import { Entity } from "@bigby/core";
+import { Game2D, Renderable2D } from "@bigby/2d";
 
-ReactDOM.render(<p>hi</p>, el);
+import { Application, with2DEditor } from "@bigby/editor";
+
+/* Get root element */
+const el = document.getElementById("app");
+
+/* Set up game */
+const game = new Entity();
+game.name = "Bigby Sample Game";
+game.addBehavior(Game2D).element = el;
+game.addBehavior(Renderable2D);
+
+/* Let's get this thing on the road */
+ReactDOM.render(<Application root={with2DEditor(game)} />, el);
