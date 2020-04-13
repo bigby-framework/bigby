a2b582650057f9eec932bcd54dcdad33a83bcdb6
Hendrik Mans <hendrik@mans.de>
2020-04-13 14:37:33 +0200

    It works!

--- packages/sample-game/src/index.tsx
+++ packages/sample-game/src/index.tsx
@@ -1,16 +1,15 @@
-// import { Application, with2DEditor } from "@bigby/editor";
+import { Application, with2DEditor } from "@bigby/editor";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-// import game from "./game";
-// import ship from "./ship";
+import game from "./game";
+import ship from "./ship";
 
 /* Get app element */
 const el = document.getElementById("app");
 
-// /* Set up game */
-// const root = game("Bigby Sample Game", el);
-// root.addChild(ship());
+/* Set up game */
+const root = game("Bigby Sample Game", el);
+root.addChild(ship());
 
-// /* Let's get this thing on the road */
-// ReactDOM.render(<Application root={with2DEditor(root)} />, el);
-ReactDOM.render(<p>hi</p>, el);
+/* Let's get this thing on the road */
+ReactDOM.render(<Application root={with2DEditor(root)} />, el);
