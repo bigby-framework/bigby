34a521c5e8cd33c652721590f6348a364d75022a
Hendrik Mans <hendrik@mans.de>
2020-04-13 16:48:17 +0200

    Add physics, random, maths

--- bundles/bigby2d/src/index.ts
+++ bundles/bigby2d/src/index.ts
@@ -1,2 +1,9 @@
 export * from "@bigby/core";
 export * from "@bigby/2d";
+export * from "@bigby/2d-physics";
+
+import * as random from "@bigby/random";
+export { random };
+
+import * as math from "@bigby/math";
+export { math };
