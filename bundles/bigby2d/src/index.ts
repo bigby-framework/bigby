8e74a668da34f89a41b27fd2f4c09d10b6b9bef4
Hendrik Mans <hendrik@mans.de>
2020-04-13 16:59:51 +0200

    Code comments

--- bundles/bigby2d/src/index.ts
+++ bundles/bigby2d/src/index.ts
@@ -1,7 +1,11 @@
+/* The purpose of this package is to provide a big object containing
+everything needed to build 2D games using Bigby. For this reason, we'll mix in
+the exports of the most important libraries at the top level: */
 export * from "@bigby/core";
 export * from "@bigby/2d";
 export * from "@bigby/2d-physics";
 
+/* ...and provide properties for some of the helper modules: */
 import * as random from "@bigby/random";
 export { random };
 
