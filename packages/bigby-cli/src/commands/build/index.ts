bebaf29ca003c8e3635e795a3642bcfea6da6b09
Hendrik Mans <hendrik@mans.de>
2020-04-14 17:47:39 +0200

    Add ora spinner

--- packages/bigby-cli/src/commands/build/index.ts
+++ packages/bigby-cli/src/commands/build/index.ts
@@ -2,12 +2,17 @@ import consola from "consola";
 import webpack from "webpack";
 import * as configuration from "../../webpack/configuration";
 import handler from "../../webpack/handler";
+import ora from "ora";
 
 export default () => {
   consola.info("Building Bigby game");
+  const spinner = ora("Building...").start();
 
+  /* Build game */
   const config = configuration.create();
   const compiler = webpack(config);
-
   compiler.run(handler);
+
+  /* Done */
+  spinner.stop();
 };
