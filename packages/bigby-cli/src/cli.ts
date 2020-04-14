260045de19903bb281295d234a8f7eb1ab531bd9
Hendrik Mans <hendrik@mans.de>
2020-04-14 12:43:41 +0200

    Add commander

--- packages/bigby-cli/src/cli.ts
+++ packages/bigby-cli/src/cli.ts
@@ -1,5 +1,10 @@
+import { program } from "commander";
 import consola from "consola";
+import pkg from "./pkg";
+
+program.version(pkg.version);
 
 export const run = () => {
   consola.info("hi from consola");
+  consola.info(pkg.version);
 };
