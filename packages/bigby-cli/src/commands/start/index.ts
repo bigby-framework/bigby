c06b58461b6a8d34a1b2cb77cce403aefc6f61e1
Hendrik Mans <hendrik@mans.de>
2020-04-14 12:56:40 +0200

    Integrate Webpack

--- packages/bigby-cli/src/commands/start/index.ts
+++ packages/bigby-cli/src/commands/start/index.ts
@@ -1,5 +1,12 @@
 import consola from "consola";
+import webpack from "webpack";
 
 export default () => {
-  consola.log("Hi from start module");
+  consola.log("Starting Bigby development server");
+
+  const compiler = webpack({});
+
+  compiler.watch({}, (err, stats) => {
+    if (err) consola.error(err);
+  });
 };
