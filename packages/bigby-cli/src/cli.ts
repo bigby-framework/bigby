d8dd1138e7aeeb107b4029926817e1c344e18e1d
Hendrik Mans <hendrik@mans.de>
2020-04-14 12:45:40 +0200

    Add commander structure

--- packages/bigby-cli/src/cli.ts
+++ packages/bigby-cli/src/cli.ts
@@ -3,8 +3,13 @@ import consola from "consola";
 import pkg from "./pkg";
 
 program.version(pkg.version);
+program
+  .command("start")
+  .description("Starts the Bigby development server and runs your game")
+  .action(() => {
+    consola.log("Starting server!");
+  });
 
 export const run = () => {
-  consola.info("hi from consola");
-  consola.info(pkg.version);
+  program.parse(process.argv);
 };
