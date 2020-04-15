ceba2fd42afd43f472ab9974a23f064b290a8bb0
Hendrik Mans <hendrik@mans.de>
2020-04-15 17:34:03 +0200

    Render README

---
+++ packages/bigby-website/pages/index.tsx
@@ -0,0 +1,18 @@
+import Layout from "../src/Layout";
+import Markdown from "react-markdown";
+import fs from "fs";
+import { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const readme = fs.readFileSync("../../README.md", "utf8").toString();
+
+  return {
+    props: { readme },
+  };
+};
+
+export default ({ readme }) => (
+  <Layout>
+    <Markdown source={readme} />
+  </Layout>
+);
