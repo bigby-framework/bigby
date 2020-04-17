ec69f196fec93142d93674b69c8a44e9ddfa7cd5
Hendrik Mans <hendrik@mans.de>
2020-04-17 17:25:03 +0200

    Cleanup

--- examples/3d-game/game/index.ts
+++ examples/3d-game/game/index.ts
@@ -1,5 +1,9 @@
 import { Game3D } from "@bigby/3d";
+import { Entity } from "@bigby/core";
+import { Ticker } from "@bigby/behaviors";
 
-const game = Game3D.make({});
+const game = new Entity();
+game.addBehavior(Ticker);
+game.addBehavior(Game3D);
 
 export default game;
--- examples/game3d-experiments/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Game3D } from "@bigby/3d";
-import "./index.css";
-
-const game = Game3D.make({
-  canvas: document.getElementById("game") as HTMLCanvasElement,
-});
-
-game.awake();
