ed77777f6f3e0ce1bd5ecc3dfc8767d2ca689772
Hendrik Mans <hendrik@mans.de>
2020-04-18 21:44:40 +0200

    Make AmbientLight3D configurable

--- packages/bigby-3d/src/AmbientLight3D.ts
+++ packages/bigby-3d/src/AmbientLight3D.ts
@@ -7,10 +7,23 @@ export default class AmbientLight3D extends Behavior {
   static icon = "💡";
   static description = "Adds ambient lighting to the scene.";
 
-  @inspect() color = 0xffffff;
-  @inspect() intensity = 0.1;
+  @inspect("Color", ["r", "g", "b"])
+  get color() {
+    return this.light.color;
+  }
+  set color({ r, g, b }) {
+    this.light.color.setRGB(r, g, b);
+  }
 
-  light = new AmbientLight(this.color, this.intensity);
+  @inspect()
+  get intensity() {
+    return this.light.intensity;
+  }
+  set intensity(v) {
+    this.light.intensity = v;
+  }
+
+  light = new AmbientLight(0xffffff, 0.5);
 
   awake() {
     this.getBehavior(Transform3D).add(this.light);
