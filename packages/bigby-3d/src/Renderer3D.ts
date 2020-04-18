40bf22b5ad56a72e23f4b3c703c3a522523cb8c3
Hendrik Mans <hendrik@mans.de>
2020-04-18 18:59:29 +0200

    It works

--- packages/bigby-3d/src/Renderer3D.ts
+++ packages/bigby-3d/src/Renderer3D.ts
@@ -1,4 +1,15 @@
 import { Behavior } from "@bigby/core";
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  Camera,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  MeshNormalMaterial,
+  Vector3,
+} from "three";
 
 class Renderer3D extends Behavior<{
   canvas: HTMLCanvasElement;
@@ -18,16 +29,52 @@ class Renderer3D extends Behavior<{
 
   set element(element) {
     this._element = element;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.element.appendChild(this.renderer.domElement);
   }
 
-  awake() {}
+  /* Scene */
+  scene: Scene;
+  camera: Camera;
+  renderer: WebGLRenderer;
+
+  awake() {
+    /* Create our scene */
+    this.scene = new Scene();
+
+    /* Create a camera */
+    this.camera = new PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    /* Add some dummy stuff */
+    var geometry = new BoxGeometry();
+    var mat = new MeshNormalMaterial({});
+    var cube = new Mesh(geometry, mat);
+    this.scene.add(cube);
+    cube.position.add(new Vector3(3, 1, 0.2));
+    this.camera.position.z = 5;
+
+    /* Create a renderer */
+    this.renderer = new WebGLRenderer();
+
+    /* Set the element (so we can launch the renderer) */
+    this.element = this.element || document.getElementById("bigby");
+  }
 
   lateUpdate() {
-    // this.scene?.render();
+    this.render();
   }
 
   editorLateUpdate() {
-    // this.scene?.render();
+    this.render();
+  }
+
+  private render() {
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
