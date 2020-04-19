7acc47a7a77f2f4521963d2da64d88119672a805
Hendrik Mans <hendrik@mans.de>
2020-04-19 12:36:14 +0200

    Random red cubes

--- packages/bigby-3d/src/CubeMesh3D.ts
+++ packages/bigby-3d/src/CubeMesh3D.ts
@@ -1,13 +1,7 @@
 import { Behavior } from "@bigby/core";
+import * as random from "@bigby/random";
+import { BoxGeometry, Mesh, MeshPhongMaterial } from "three";
 import Transform3D from "./Transform3D";
-import {
-  BoxGeometry,
-  MeshNormalMaterial,
-  Mesh,
-  MeshLambertMaterial,
-  MeshPhongMaterial,
-  MeshBasicMaterial,
-} from "three";
 
 export default class CubeMesh3D extends Behavior {
   static displayName = "CubeMesh3D";
@@ -19,29 +13,12 @@ export default class CubeMesh3D extends Behavior {
   awake() {
     /* Add some dummy stuff */
     const phongMat = new MeshPhongMaterial({
-      color: 0x000000,
+      color: Math.floor(random.number(0xff)) * 0xff0000,
       specular: 0x666666,
-      emissive: 0xff0000,
       shininess: 10,
-      opacity: 0.9,
-      transparent: true,
     });
 
-    const phongMat2 = new MeshPhongMaterial({
-      color: 0xdddddd,
-      specular: 0xffffff,
-      shininess: 30,
-      flatShading: true,
-    });
-
-    const basicMat = new MeshBasicMaterial({
-      color: 0xffaa00,
-      wireframe: true,
-    });
-
-    const lambertMat = new MeshLambertMaterial({ color: 0xdddddd });
-
-    const cube = new Mesh(CubeMesh3D.geometry, phongMat2);
+    const cube = new Mesh(CubeMesh3D.geometry, phongMat);
 
     /* Add the cube to our local Transform3D */
     this.getBehavior(Transform3D).add(cube);
