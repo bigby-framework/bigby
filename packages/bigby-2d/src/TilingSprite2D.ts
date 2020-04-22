a09f247bb063d7ca40313717e3fe2c0020e6a202
Hendrik Mans <hendrik@mans.de>
2020-04-22 18:55:15 +0200

    Reimplement TilingSprite2D

--- packages/bigby-2d/src/TilingSprite2D.ts
+++ packages/bigby-2d/src/TilingSprite2D.ts
@@ -1,13 +1,24 @@
-import { inspect } from "@bigby/core";
+import { inspect, Behavior } from "@bigby/core";
 import * as PIXI from "pixi.js";
-import Sprite2D from "./Sprite2D";
 
-export default class TilingSprite2D extends Sprite2D<{
+export default class TilingSprite2D extends Behavior<{
   bounds: { width: number; height: number };
 }> {
+  static icon = "🚀";
   static displayName = "TilingSprite2D";
   static description =
     "Renders a tiling 2D sprite. The specified texture is repeated across the dimensions of this sprite.";
+
+  /* Texture URI */
+  @inspect("Texture URI")
+  get uri() {
+    return this._uri;
+  }
+  set uri(uri) {
+    this._uri = uri;
+    this.createSprite(PIXI.Texture.from(uri));
+  }
+  private _uri: string;
 
   @inspect("Size", ["width", "height"])
   bounds = { width: 500, height: 500 };
@@ -15,17 +26,21 @@ export default class TilingSprite2D extends Sprite2D<{
   @inspect("Tile Scale", ["x", "y"])
   tileScale = { x: 1, y: 1 };
 
-  protected sprite: PIXI.TilingSprite;
+  private sprite: PIXI.TilingSprite;
 
-  createSpriteFromTexture() {
-    return new PIXI.TilingSprite(
-      this.texture,
+  createSprite(texture: PIXI.Texture) {
+    if (this.sprite) this.sprite.destroy();
+
+    this.sprite = new PIXI.TilingSprite(
+      texture,
       this.bounds.width,
       this.bounds.height
     );
   }
 
   update() {
+    if (!this.sprite) return;
+
     this.sprite.width = this.bounds.width;
     this.sprite.height = this.bounds.height;
     this.sprite.tileScale.set(this.tileScale.x, this.tileScale.y);
