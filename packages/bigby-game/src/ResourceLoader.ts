522b9f25149e1d975182e37fdf85366ff27f6b38
Hendrik Mans <hendrik@mans.de>
2020-05-01 18:43:23 +0200

    ResourceLoader.loader

--- packages/bigby-game/src/ResourceLoader.ts
+++ packages/bigby-game/src/ResourceLoader.ts
@@ -2,9 +2,7 @@ import { Behavior } from "@bigby/core";
 import * as PIXI from "pixi.js";
 
 export default class ResourceLoader extends Behavior {
-  private loader?: PIXI.Loader;
-
-  awake() {
-    this.loader = PIXI.Loader.shared;
+  get loader() {
+    return PIXI.Loader.shared;
   }
 }
