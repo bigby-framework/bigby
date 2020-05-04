e6d63d9bb0d3d2df285c22e24026c0ebae428edf
Hendrik Mans <hendrik@mans.de>
2020-05-04 20:43:55 +0200

    Add some (messy) memoization

--- packages/bigby-game/src/GameBehavior.ts
+++ packages/bigby-game/src/GameBehavior.ts
@@ -1,21 +1,29 @@
 import { Behavior } from "@bigby/core";
 import Transform from "./Transform";
 import ResourceLoader from "./ResourceLoader";
+import * as PIXI from "pixi.js";
 
 /**
  * A subclass of the original [[Behavior]] class with some extra convenience
  * functionality specific to @bigby/game.
  */
 export default class GameBehavior extends Behavior {
+  /* TODO: find a nicer, less noisy way to memoize these. */
+
+  private __transform?: Transform;
   get transform() {
-    return this.getBehavior(Transform);
+    return (this.__transform = this.__transform || this.getBehavior(Transform));
   }
 
+  private __nearestTransform?: Transform;
   get nearestTransform() {
-    return this.getNearestBehavior(Transform);
+    return (this.__nearestTransform =
+      this.__nearestTransform || this.getNearestBehavior(Transform));
   }
 
+  private __loader?: PIXI.Loader;
   get loader() {
-    return this.getNearestBehavior(ResourceLoader)!.loader;
+    return (this.__loader =
+      this.__loader || this.getNearestBehavior(ResourceLoader)!.loader);
   }
 }
