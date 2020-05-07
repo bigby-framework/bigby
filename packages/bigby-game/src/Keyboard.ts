4b031da3e374b2f4717d22b77f70bc5b98508613
Hendrik Mans <hendrik@mans.de>
2020-05-07 11:47:30 +0200

    Comments

--- packages/bigby-game/src/Keyboard.ts
+++ packages/bigby-game/src/Keyboard.ts
@@ -9,6 +9,15 @@ hotkeys(" ", () => {});
  * Provides an interface to the user's keyboard.
  */
 export default class Keyboard extends Behavior {
+  /**
+   * Returns true if the specified key is currently being pressed. This behavior
+   * uses `hotkeys-js` under the hood, so you can use any key codes supported by
+   * it. (See its [documentation](https://github.com/jaywcjlove/hotkeys) for
+   * details.)
+   *
+   * @readonly
+   * @memberof Keyboard
+   */
   get isPressed() {
     return hotkeys.isPressed;
   }
