f28ba30694ee07ba526701cbd1df9e9a6170efd5
Hendrik Mans <hendrik@mans.de>
2020-05-07 12:18:48 +0200

    StatsPanel

---
+++ packages/bigby-game/src/StatsPanel.ts
@@ -0,0 +1,30 @@
+import { Behavior } from "@bigby/core";
+import Stats from "stats.js";
+import Ticker from "./Ticker";
+
+/**
+ * Provides a runtime statistics panel powered by mrdoob's excellent Stats.js.
+ * You will typically want to put this into your root entity alongside your
+ * Renderer and Ticker behaviors.
+ */
+export default class StatsPanel extends Behavior {
+  private stats = new Stats();
+
+  awake() {
+    document.body.appendChild(this.stats.dom);
+    this.stats.showPanel(1);
+
+    /* Look for a nearby Ticker instance and hook into it. */
+    const ticker = this.getNearestBehavior(Ticker);
+    if (ticker) {
+      ticker.beforeTick(() => this.stats.begin());
+      ticker.afterTick(() => this.stats.end());
+    } else {
+      console.warn("StatsPanel requires a Ticker instance to function.");
+    }
+  }
+
+  destroy() {
+    document.body.removeChild(this.stats.dom);
+  }
+}
--- packages/bigby-game/src/index.ts
+++ packages/bigby-game/src/index.ts
@@ -3,11 +3,11 @@ import Keyboard from "./Keyboard";
 import Renderer from "./Renderer";
 import ResourceLoader from "./ResourceLoader";
 import Sprite from "./Sprite";
+import StatsPanel from "./StatsPanel";
 import Ticker from "./Ticker";
 import Transform from "./Transform";
 
 export * as vec2 from "./vec2";
-
 export {
   GameBehavior,
   Renderer,
@@ -16,4 +16,5 @@ export {
   Sprite,
   ResourceLoader,
   Keyboard,
+  StatsPanel,
 };
