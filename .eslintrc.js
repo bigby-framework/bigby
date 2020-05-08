54a73ddd2ff3629b37b5f203a33e0a1f25616686
Hendrik Mans <hendrik@mans.de>
2020-05-08 14:43:45 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {},
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
--- packages/bigby-core/src/Behavior.ts
+++ packages/bigby-core/src/Behavior.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import Entity from "./Entity";
 
 export type BehaviorConstructor<T extends Behavior = Behavior> = new (
