fde9900ab4303082f9dc18c587cd908c94ec6158
Hendrik Mans <hendrik@mans.de>
2020-05-08 16:15:17 +0200

    Comments

--- packages/bigby-game/src/TilingSprite.ts
+++ packages/bigby-game/src/TilingSprite.ts
@@ -1,6 +1,15 @@
 import * as PIXI from "pixi.js";
 import Sprite from "./Sprite";
 
+/**
+ * A special type of sprite that can render a tiled texture. Declare the
+ * TilingSprite's dimensions via the [[bounds]] property or change the tile
+ * scale through [[tileScale]].
+ *
+ * @export
+ * @class TilingSprite
+ * @extends {Sprite}
+ */
 export default class TilingSprite extends Sprite {
   bounds = { width: 512, height: 512 };
   tileScale = { x: 1, y: 1 };
