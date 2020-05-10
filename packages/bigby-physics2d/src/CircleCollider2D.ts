37996fff7cb8760ddbc67c8b0e108bdcb150e91d
Hendrik Mans <hendrik@mans.de>
2020-05-10 12:36:43 +0200

    Implement CircleCollider2D

--- packages/bigby-physics2d/src/CircleCollider2D.ts
+++ packages/bigby-physics2d/src/CircleCollider2D.ts
@@ -1,3 +1,29 @@
 import { Behavior } from "@bigby/core";
+import { RigidBody2D } from ".";
+import * as planck from "planck-js";
 
-export default class CircleCollider2D extends Behavior {}
+export default class CircleCollider2D extends Behavior {
+  radius = 1;
+  friction = 0;
+  density = 1;
+
+  awake() {
+    const rb2d = this.$(RigidBody2D);
+
+    if (!rb2d) {
+      console.error("CircleCollider2D needs a RigidBody2D to operate.");
+      return;
+    }
+
+    if (!rb2d.body) {
+      console.error("The entity's RigidBody2D needs to be awake.");
+      return;
+    }
+
+    rb2d.body.createFixture({
+      shape: planck.Circle(this.radius),
+      friction: this.friction,
+      density: this.density,
+    });
+  }
+}
