0cc743f88d32d109018885a55f38ce991e141894
Hendrik Mans <hendrik@mans.de>
2020-05-11 11:54:57 +0200

    Export logger in bigby

--- packages/bigby/src/index.ts
+++ packages/bigby/src/index.ts
@@ -1,4 +1,5 @@
 export * from "@bigby/core";
 export * as game from "@bigby/game";
+export * as logger from "@bigby/logger";
 export * as physics2d from "@bigby/physics2d";
 export * as random from "@bigby/random";
