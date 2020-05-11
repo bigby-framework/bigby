d53eab616834fc829aa6ad974326adb2ca34b9fd
Hendrik Mans <hendrik@mans.de>
2020-05-11 18:25:00 +0200

    Add PolygonCollider2D

---
+++ packages/bigby-physics2d/src/PolygonCollider2D.ts
@@ -0,0 +1,15 @@
+import * as planck from "planck-js";
+import AbstractCollider2D from "./AbstractCollider2D";
+import { vec2 } from "@bigby/game";
+
+export default class PolygonCollider2D extends AbstractCollider2D {
+  points: vec2.IVec2[] = [];
+
+  protected fixtureShape() {
+    return planck.Polygon(
+      this.points.map((point) =>
+        planck.Vec2(point.x / this.p2d!.ppu, point.y / this.p2d!.ppu)
+      )
+    );
+  }
+}
--- packages/bigby-physics2d/src/index.ts
+++ packages/bigby-physics2d/src/index.ts
@@ -1,5 +1,6 @@
 import CircleCollider2D from "./CircleCollider2D";
 import Physics2D from "./Physics2D";
+import PolygonCollider2D from "./PolygonCollider2D";
 import RigidBody2D from "./RigidBody2D";
 
-export { Physics2D, RigidBody2D, CircleCollider2D };
+export { Physics2D, RigidBody2D, CircleCollider2D, PolygonCollider2D };
