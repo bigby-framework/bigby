59a6607a3c8aeda1ca64b82c91ae37d71f9a5f1f
Hendrik Mans <hendrik@mans.de>
2020-05-11 19:07:14 +0200

    Add PhysicsDataLoader

---
+++ packages/bigby-physics2d/src/PhysicsDataLoader2D.ts
@@ -0,0 +1,39 @@
+import { GameBehavior, vec2 } from "@bigby/game";
+import { IVec2 } from "@bigby/game/dist/vec2";
+import { $ } from "@bigby/core";
+import RigidBody2D from "./RigidBody2D";
+import PolygonCollider2D from "./PolygonCollider2D";
+
+export type Filter = {
+  categoryBits: number;
+  maskBits: number;
+};
+
+export type Fixture = {
+  density: number;
+  friction: number;
+  bounce: number;
+  filter: Filter;
+  points: IVec2[];
+};
+
+export type PhysicsData = Fixture[];
+
+export default class PhysicsDataLoader2D extends GameBehavior {
+  data?: PhysicsData;
+
+  awake() {
+    /* Create a RigidBody2D behavior if it's not already there */
+    if (!$(this, RigidBody2D)) this.entity.addBehavior(RigidBody2D);
+
+    /* Create colliders for each fixture */
+    this.data?.forEach((fixture) => {
+      this.entity.addBehavior(PolygonCollider2D, {
+        density: fixture.density,
+        friction: fixture.friction,
+        restitution: fixture.bounce,
+        points: fixture.points,
+      });
+    });
+  }
+}
--- packages/bigby-physics2d/src/index.ts
+++ packages/bigby-physics2d/src/index.ts
@@ -1,6 +1,13 @@
 import CircleCollider2D from "./CircleCollider2D";
 import Physics2D from "./Physics2D";
+import PhysicsDataLoader2D from "./PhysicsDataLoader2D";
 import PolygonCollider2D from "./PolygonCollider2D";
 import RigidBody2D from "./RigidBody2D";
 
-export { Physics2D, RigidBody2D, CircleCollider2D, PolygonCollider2D };
+export {
+  Physics2D,
+  PhysicsDataLoader2D,
+  RigidBody2D,
+  CircleCollider2D,
+  PolygonCollider2D,
+};
