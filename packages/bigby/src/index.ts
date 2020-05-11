a23f3c567a3fb4a94bcdb7611e38acae8339e9c4
Hendrik Mans <hendrik@mans.de>
2020-05-11 17:08:41 +0200

    Export timers

--- packages/bigby/src/index.ts
+++ packages/bigby/src/index.ts
@@ -3,3 +3,4 @@ export * as game from "@bigby/game";
 export * as logger from "@bigby/logger";
 export * as physics2d from "@bigby/physics2d";
 export * as random from "@bigby/random";
+export * as timers from "@bigby/timers";
