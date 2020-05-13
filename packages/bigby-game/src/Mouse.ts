5ece651275e0856a087e45d48176956f725881ca
Hendrik Mans <hendrik@mans.de>
2020-05-13 13:32:02 +0200

    Have mouse report position and button states

--- packages/bigby-game/src/Mouse.ts
+++ packages/bigby-game/src/Mouse.ts
@@ -1,6 +1,9 @@
 import { $, Behavior } from "@bigby/core";
 import * as PIXI from "pixi.js";
 import Renderer from "./Renderer";
+import Transform from "./Transform";
+
+type MouseButton = 1 | 2 | 4;
 
 export default class Mouse extends Behavior {
   private renderer?: Renderer;
@@ -9,5 +12,29 @@ export default class Mouse extends Behavior {
   awake() {
     this.renderer = $(this, Renderer);
     this.interaction = this.renderer!.app!.renderer.plugins.interaction;
+  }
+
+  getButtons() {
+    return this.interaction!.mouse.buttons;
+  }
+
+  getPosition(origin?: Transform) {
+    return this.interaction!.mouse.getLocalPosition(this.renderer!.app!.stage);
+  }
+
+  isButtonPressed(button: MouseButton) {
+    return (this.getButtons() & button) == button;
+  }
+
+  isLeftButtonPressed() {
+    return this.isButtonPressed(1);
+  }
+
+  isRightButtonPressed() {
+    return this.isButtonPressed(2);
+  }
+
+  isMouseWheelPressed() {
+    return this.isButtonPressed(4);
   }
 }
